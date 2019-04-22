f9fece9feb07189c45e5a50ff58f11d4a315e4e4
Manjunath Ks <manjunath.ks@synapse.com>
2019-04-22 11:28:55 +0530

    code update of faceboon login page loader

--- src/test/java/jenkins/MavenTest/HelloTest.java
+++ src/test/java/jenkins/MavenTest/HelloTest.java
@@ -1,11 +1,17 @@
 package jenkins.MavenTest;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 public class HelloTest extends TestCase
 {
+	
+	String path = System.getProperty("user.dir");
+	
     /**
      * Create the test case
      *
@@ -28,10 +34,22 @@ public class HelloTest extends TestCase
 
     /**
      * Rigourous Test :-)
+     * @throws InterruptedException 
      */
-    public void testApp()
+    public void testApp() throws InterruptedException
     {
     	System.out.println("Hello Test suite method testApp");
+    	
+    	System.setProperty("webdriver.chrome.driver", path + "\\drivers\\chromedriver.exe");
+    	
+    	// Initialize browser
+    	WebDriver driver=new ChromeDriver();
+    	// Open facebook
+    	driver.get("http://www.facebook.com");
+    	Thread.sleep(2000);
+    	// Maximize browser
+    	driver.manage().window().maximize();
+    	driver.quit();
         assertTrue( true );
     }
 	
